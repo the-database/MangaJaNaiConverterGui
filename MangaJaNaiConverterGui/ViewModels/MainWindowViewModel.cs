0c1960e7a62a0a1608eee98a27f8821b2d4ac14f
the-database <25811902+the-database@users.noreply.github.com>
2023-10-15 16:59:46 -0400

    ui progress

---
+++ MangaJaNaiConverterGui/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,111 @@
+﻿using ReactiveUI;
+using System.Runtime.Serialization;
+
+namespace MangaJaNaiConverterGui.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+
+        private int _selectedTabIndex;
+        [DataMember]
+        public int SelectedTabIndex
+        {
+            get => _selectedTabIndex;
+            set
+            {
+                if (_selectedTabIndex != value)
+                {
+                    this.RaiseAndSetIfChanged(ref _selectedTabIndex, value);
+                }
+            }
+        }
+
+        private string _inputFilePath = string.Empty;
+        [DataMember]
+        public string InputFilePath
+        {
+            get => _inputFilePath;
+            set => this.RaiseAndSetIfChanged(ref _inputFilePath, value);
+        }
+
+        private string _inputFolderPath = string.Empty;
+        [DataMember]
+        public string InputFolderPath
+        {
+            get => _inputFolderPath;
+            set => this.RaiseAndSetIfChanged(ref _inputFolderPath, value);
+        }
+
+        private string _outputFilePath = string.Empty;
+        [DataMember]
+        public string OutputFilePath
+        {
+            get => _outputFilePath;
+            set => this.RaiseAndSetIfChanged(ref _outputFilePath, value);
+        }
+
+        private string _outputFolderPath = string.Empty;
+        [DataMember]
+        public string OutputFolderPath
+        {
+            get => _outputFolderPath;
+            set => this.RaiseAndSetIfChanged(ref _outputFolderPath, value);
+        }
+
+        private bool _autoAdjustLevels = false;
+        [DataMember]
+        public bool AutoAdjustLevels
+        {
+            get => _autoAdjustLevels;
+            set => this.RaiseAndSetIfChanged(ref _autoAdjustLevels, value);
+        }
+
+        private string _grayscaleModelFilePath = string.Empty;
+        [DataMember]
+        public string GrayscaleModelFilePath
+        {
+            get => _grayscaleModelFilePath;
+            set => this.RaiseAndSetIfChanged(ref _grayscaleModelFilePath, value);
+        }
+
+        private string _colorModelFilePath = string.Empty;
+        [DataMember]
+        public string ColorModelFilePath
+        {
+            get => _colorModelFilePath;
+            set => this.RaiseAndSetIfChanged(ref _colorModelFilePath, value);
+        }
+
+        private string _resizeHeightBeforeUpscale = 0.ToString();
+        [DataMember]
+        public string ResizeHeightBeforeUpscale
+        {
+            get => _resizeHeightBeforeUpscale;
+            set => this.RaiseAndSetIfChanged(ref _resizeHeightBeforeUpscale, value);
+        }
+
+        private string _resizeFactorBeforeUpscale = 100.ToString();
+        [DataMember]
+        public string ResizeFactorBeforeUpscale
+        {
+            get => _resizeFactorBeforeUpscale;
+            set => this.RaiseAndSetIfChanged(ref _resizeFactorBeforeUpscale, value);
+        }
+
+        private string _resizeHeightAfterUpscale = 0.ToString();
+        [DataMember]
+        public string ResizeHeightAfterUpscale
+        {
+            get => _resizeHeightAfterUpscale;
+            set => this.RaiseAndSetIfChanged(ref _resizeHeightAfterUpscale, value);
+        }
+
+        private string _resizeFactorAfterUpscale = 100.ToString();
+        [DataMember]
+        public string ResizeFactorAfterUpscale
+        {
+            get => _resizeFactorAfterUpscale;
+            set => this.RaiseAndSetIfChanged(ref _resizeFactorAfterUpscale, value);
+        }
+    }
+}
---
+++ MangaJaNaiConverterGui/Views/MainWindow.axaml.cs
@@ -0,0 +1,331 @@
+using MangaJaNaiConverterGui.ViewModels;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+using Avalonia.Platform.Storage;
+using Avalonia.ReactiveUI;
+using ReactiveUI;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace MangaJaNaiConverterGui.Views
+{
+    public partial class MainWindow : ReactiveWindow<MainWindowViewModel>
+    {
+        private bool _autoScrollConsole = true;
+
+        public MainWindow()
+        {
+            //InitializeComponent();
+            AvaloniaXamlLoader.Load(this);
+            this.WhenActivated(disposable => { });
+            Resized += MainWindow_Resized;
+            Closing += MainWindow_Closing;
+
+            var inputFileNameTextBox = this.FindControl<TextBox>("InputFileNameTextBox");
+            var outputFileNameTextBox = this.FindControl<TextBox>("OutputFileNameTextBox");
+            var inputFolderNameTextBox = this.FindControl<TextBox>("InputFolderNameTextBox");
+            var outputFolderNameTextBox = this.FindControl<TextBox>("OutputFolderNameTextBox");
+
+            inputFileNameTextBox?.AddHandler(DragDrop.DropEvent, SetInputFilePath);
+            outputFileNameTextBox?.AddHandler(DragDrop.DropEvent, SetOutputFilePath);
+            inputFolderNameTextBox?.AddHandler(DragDrop.DropEvent, SetInputFolderPath);
+            outputFolderNameTextBox?.AddHandler(DragDrop.DropEvent, SetOutputFolderPath);
+        }
+
+        private void MainWindow_Closing(object? sender, WindowClosingEventArgs e)
+        {
+            if (DataContext is MainWindowViewModel vm)
+            {
+                //vm.CancelUpscale();
+            }
+        }
+
+        private void ConsoleScrollViewer_PropertyChanged(object? sender, AvaloniaPropertyChangedEventArgs e)
+        {
+            if (e.Property.Name == "Offset")
+            {
+                var consoleScrollViewer = this.FindControl<ScrollViewer>("ConsoleScrollViewer");
+
+                if (e.NewValue is Vector newVector)
+                {
+                    _autoScrollConsole = newVector.Y == consoleScrollViewer?.ScrollBarMaximum.Y;
+                }
+            }
+
+        }
+
+        private void ConsoleTextBlock_PropertyChanged(object? sender, AvaloniaPropertyChangedEventArgs e)
+        {
+            if (e.Property.Name == "Text")
+            {
+                var consoleScrollViewer = this.FindControl<ScrollViewer>("ConsoleScrollViewer");
+                if (consoleScrollViewer != null)
+                {
+                    if (_autoScrollConsole)
+                    {
+                        consoleScrollViewer.ScrollToEnd();
+                    }
+                }
+            }
+        }
+
+        private void MainWindow_Resized(object? sender, WindowResizedEventArgs e)
+        {
+            // Set the ScrollViewer width based on the new parent window's width
+            var consoleScrollViewer = this.FindControl<ScrollViewer>("ConsoleScrollViewer");
+            if (consoleScrollViewer != null)
+            {
+                consoleScrollViewer.Width = Width - 40; // Adjust the width as needed
+            }
+
+        }
+
+        private async void OpenInputFileButtonClick(object? sender, RoutedEventArgs e)
+        {
+            // Get top level from the current control. Alternatively, you can use Window reference instead.
+            var topLevel = TopLevel.GetTopLevel(this);
+
+            // Start async operation to open the dialog.
+            var files = await topLevel.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
+            {
+                Title = "Open Video File",
+                AllowMultiple = false
+            });
+
+            if (files.Count >= 1)
+            {
+                //// Open reading stream from the first file.
+                //await using var stream = await files[0].OpenReadAsync();
+                //using var streamReader = new StreamReader(stream);
+                //// Reads all the content of file as a text.
+                //var fileContent = await streamReader.ReadToEndAsync();
+                if (DataContext is MainWindowViewModel vm)
+                {
+                    vm.InputFilePath = files[0].TryGetLocalPath() ?? "";
+                }
+            }
+        }
+
+        public void SetInputFilePath(object? sender, DragEventArgs e)
+        {
+            if (DataContext is MainWindowViewModel vm)
+            {
+                var files = e.Data.GetFiles().ToList();
+
+
+                if (files.Count > 0)
+                {
+                    var filePath = files[0].TryGetLocalPath();
+                    if (File.Exists(filePath))
+                    {
+                        vm.InputFilePath = filePath;
+                    }
+                }
+            }
+        }
+
+        public void SetOutputFilePath(object? sender, DragEventArgs e)
+        {
+            if (DataContext is MainWindowViewModel vm)
+            {
+                var files = e.Data.GetFiles().ToList();
+
+
+                if (files.Count > 0)
+                {
+                    var filePath = files[0].TryGetLocalPath();
+                    if (File.Exists(filePath))
+                    {
+                        vm.OutputFilePath = filePath;
+                    }
+                }
+            }
+        }
+
+        public void SetInputFolderPath(object? sender, DragEventArgs e)
+        {
+            if (DataContext is MainWindowViewModel vm)
+            {
+                var files = e.Data.GetFiles().ToList();
+
+
+                if (files.Count > 0)
+                {
+                    var filePath = files[0].TryGetLocalPath();
+                    if (Directory.Exists(filePath))
+                    {
+                        vm.InputFolderPath = filePath;
+                    }
+                }
+            }
+        }
+
+        public void SetOutputFolderPath(object? sender, DragEventArgs e)
+        {
+            if (DataContext is MainWindowViewModel vm)
+            {
+                var files = e.Data.GetFiles().ToList();
+
+
+                if (files.Count > 0)
+                {
+                    var filePath = files[0].TryGetLocalPath();
+                    if (Directory.Exists(filePath))
+                    {
+                        vm.OutputFolderPath = filePath;
+                    }
+                }
+            }
+        }
+
+        private async void OpenOutputFileButtonClick(object? sender, RoutedEventArgs e)
+        {
+            // Get top level from the current control. Alternatively, you can use Window reference instead.
+            var topLevel = TopLevel.GetTopLevel(this);
+
+            // Start async operation to open the dialog.
+            var file = await topLevel.StorageProvider.SaveFilePickerAsync(new FilePickerSaveOptions
+            {
+                Title = "Save Video File",
+                DefaultExtension = "mkv",
+                FileTypeChoices = new FilePickerFileType[]
+                {
+                    new("MKV file (*.mkv)") { Patterns = new[] { "*.mkv" } },
+                    new("MP4 file (*.mp4)") { Patterns = new[] { "*.mp4" } },
+                    new("All files (*.*)") { Patterns = new[] { "*" } },
+                },
+            });
+
+            if (file is not null)
+            {
+                //// Open reading stream from the first file.
+                //await using var stream = await files[0].OpenReadAsync();
+                //using var streamReader = new StreamReader(stream);
+                //// Reads all the content of file as a text.
+                //var fileContent = await streamReader.ReadToEndAsync();
+                if (DataContext is MainWindowViewModel vm)
+                {
+                    vm.OutputFilePath = file.TryGetLocalPath() ?? "";
+                }
+            }
+        }
+
+        private async void OpenInputFolderButtonClick(object? sender, RoutedEventArgs e)
+        {
+            // Get top level from the current control. Alternatively, you can use Window reference instead.
+            var topLevel = TopLevel.GetTopLevel(this);
+
+            // Start async operation to open the dialog.
+            var files = await topLevel.StorageProvider.OpenFolderPickerAsync(new FolderPickerOpenOptions
+            {
+                Title = "Open Folder",
+                AllowMultiple = false
+            });
+
+            if (files.Count >= 1)
+            {
+                //// Open reading stream from the first file.
+                //await using var stream = await files[0].OpenReadAsync();
+                //using var streamReader = new StreamReader(stream);
+                //// Reads all the content of file as a text.
+                //var fileContent = await streamReader.ReadToEndAsync();
+                if (DataContext is MainWindowViewModel vm)
+                {
+                    vm.InputFolderPath = files[0].TryGetLocalPath() ?? "";
+                }
+            }
+        }
+
+        private async void OpenOutputFolderButtonClick(object? sender, RoutedEventArgs e)
+        {
+            // Get top level from the current control. Alternatively, you can use Window reference instead.
+            var topLevel = TopLevel.GetTopLevel(this);
+
+            // Start async operation to open the dialog.
+            var files = await topLevel.StorageProvider.OpenFolderPickerAsync(new FolderPickerOpenOptions
+            {
+                Title = "Open Folder",
+                AllowMultiple = false
+            });
+
+            if (files.Count >= 1)
+            {
+                //// Open reading stream from the first file.
+                //await using var stream = await files[0].OpenReadAsync();
+                //using var streamReader = new StreamReader(stream);
+                //// Reads all the content of file as a text.
+                //var fileContent = await streamReader.ReadToEndAsync();
+                if (DataContext is MainWindowViewModel vm)
+                {
+                    vm.OutputFolderPath = files[0].TryGetLocalPath() ?? "";
+                }
+            }
+        }
+
+        private async void OpenGrayscaleModelFileButtonClick(object? sender, RoutedEventArgs e)
+        {
+            // Get top level from the current control. Alternatively, you can use Window reference instead.
+            var topLevel = TopLevel.GetTopLevel(this);
+
+            // Start async operation to open the dialog.
+            var files = await topLevel.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
+            {
+                Title = "Open Video File",
+                AllowMultiple = false,
+                FileTypeFilter = new FilePickerFileType[] 
+                { 
+                    new("Model File") { Patterns = new[] { "*.pth", "*.pt", "*.ckpt", "*.onnx" }, MimeTypes = new[] { "*/*" } }, FilePickerFileTypes.All,
+                },
+
+            });
+
+            if (files.Count >= 1)
+            {
+                //// Open reading stream from the first file.
+                //await using var stream = await files[0].OpenReadAsync();
+                //using var streamReader = new StreamReader(stream);
+                //// Reads all the content of file as a text.
+                //var fileContent = await streamReader.ReadToEndAsync();
+                if (DataContext is MainWindowViewModel vm)
+                {
+                    vm.GrayscaleModelFilePath = files[0].TryGetLocalPath() ?? "";
+                }
+            }
+        }
+
+        private async void OpenColorModelFileButtonClick(object? sender, RoutedEventArgs e)
+        {
+            // Get top level from the current control. Alternatively, you can use Window reference instead.
+            var topLevel = TopLevel.GetTopLevel(this);
+
+            // Start async operation to open the dialog.
+            var files = await topLevel.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
+            {
+                Title = "Open Video File",
+                AllowMultiple = false,
+                FileTypeFilter = new FilePickerFileType[]
+                {
+                    new("Model File") { Patterns = new[] { "*.pth", "*.pt", "*.ckpt", "*.onnx" }, MimeTypes = new[] { "*/*" } }, FilePickerFileTypes.All,
+                },
+            });
+
+            if (files.Count >= 1)
+            {
+                //// Open reading stream from the first file.
+                //await using var stream = await files[0].OpenReadAsync();
+                //using var streamReader = new StreamReader(stream);
+                //// Reads all the content of file as a text.
+                //var fileContent = await streamReader.ReadToEndAsync();
+                if (DataContext is MainWindowViewModel vm)
+                {
+                    vm.ColorModelFilePath = files[0].TryGetLocalPath() ?? "";
+                }
+            }
+        }
+    }
+}
