556656e4c4a4be2e48ba9a1a6c297f2800bd5a2d
the-database <25811902+the-database@users.noreply.github.com>
2023-10-16 22:20:21 -0400

    persistent app state

--- MangaJaNaiConverterGui/App.axaml.cs
+++ MangaJaNaiConverterGui/App.axaml.cs
@@ -1,8 +1,10 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using Avalonia.ReactiveUI;
 using MangaJaNaiConverterGui.ViewModels;
 using MangaJaNaiConverterGui.Views;
+using ReactiveUI;
 
 namespace MangaJaNaiConverterGui
 {
@@ -15,14 +17,14 @@ namespace MangaJaNaiConverterGui
 
         public override void OnFrameworkInitializationCompleted()
         {
-            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
-            {
-                desktop.MainWindow = new MainWindow
-                {
-                    DataContext = new MainWindowViewModel(),
-                };
-            }
-
+            var suspension = new AutoSuspendHelper(ApplicationLifetime);
+            RxApp.SuspensionHost.CreateNewAppState = () => new MainWindowViewModel();
+            RxApp.SuspensionHost.SetupDefaultSuspendResume(new NewtonsoftJsonSuspensionDriver("appstate.json"));
+            suspension.OnFrameworkInitializationCompleted();
+            
+            // Load the saved view model state.
+            var state = RxApp.SuspensionHost.GetAppState<MainWindowViewModel>();
+            new MainWindow { DataContext = state }.Show();
             base.OnFrameworkInitializationCompleted();
         }
     }
