e652e05b08b4ccb5456aa49bdf5d647cf3e437c3
the-database <25811902+the-database@users.noreply.github.com>
2024-07-19 17:18:34 -0400

    cleanup

--- MangaJaNaiConverterGui/backend/src/custom_types.py
+++
@@ -1,5 +0,0 @@
-from __future__ import annotations
-
-from typing import Awaitable, Callable, Union
-
-UpdateProgressFn = Callable[[str, float, Union[float, None]], Awaitable[None]]
--- MangaJaNaiConverterGui/backend/src/run.py
+++
@@ -1,8 +0,0 @@
-import importlib
-
-# Install server dependencies. Can't start the server without them, but we don't want to install the other deps yet.
-importlib.import_module("dependencies.install_server_deps")
-
-# Start the host server
-server_host = importlib.import_module("server_host")
-server_host.main()
