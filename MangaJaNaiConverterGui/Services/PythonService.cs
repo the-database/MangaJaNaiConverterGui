af5581bf05a500797ee177492e090db45bd2b942
the-database <25811902+the-database@users.noreply.github.com>
2024-07-25 12:38:38 -0400

    use fixed versions for all deps

--- MangaJaNaiConverterGui/Services/PythonService.cs
+++ MangaJaNaiConverterGui/Services/PythonService.cs
@@ -134,21 +134,21 @@ namespace MangaJaNaiConverterGui.Services
             get
             {
                 string[] dependencies = {
-                    "spandrel^>=0.3.4",
-                    "spandrel_extra_arches^>=0.1.1",
-                    "opencv-python^>=4.10.0.84",
-                    "pillow-avif-plugin^>=1.4.6",
-                    "rarfile^>=4.2",
-                    "multiprocess^>=0.70.16",
-                    "chainner_ext^>=0.3.10",
-                    "sanic^>=24.6.0",
-                    "pynvml^>=11.5.3",
-                    "psutil^>=6.0.0"
+                    "spandrel==0.3.4",
+                    "spandrel_extra_arches==0.1.1",
+                    "opencv-python==4.10.0.84",
+                    "pillow-avif-plugin==1.4.6",
+                    "rarfile==4.2",
+                    "multiprocess==0.70.16",
+                    "chainner_ext==0.3.10",
+                    "sanic==24.6.0",
+                    "pynvml==11.5.3",
+                    "psutil==6.0.0"
                 };
 
                 var relPythonPath = @".\python\python\python.exe";
 
-                return $@"{relPythonPath} -m pip install torch==2.3.1 torchvision^>=0.18.1 --index-url https://download.pytorch.org/whl/cu121 && {relPythonPath} -m pip install {string.Join(" ", dependencies)}";
+                return $@"{relPythonPath} -m pip install torch==2.3.1 torchvision==0.18.1 --index-url https://download.pytorch.org/whl/cu121 && {relPythonPath} -m pip install {string.Join(" ", dependencies)}";
             }
         }
 
